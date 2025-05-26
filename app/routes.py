a57edc5712ae9e4049cb68bd4393196ef1a43ef0
Siedin Ostap <sied0stap159@gmail.com>
2025-05-26 17:53:28 +0200

    26.05.2025

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,34 @@
-from flask import render_template
 from app import app
+from flask import render_template, redirect, url_for, request
+from app.forms import ExtractForm
+from app.models import Products
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/extract")
+def display_form():
+    form = ExtractForm()
+    return render_template("extract.html", form=form)
+
+@app.route("/extract", methods=['POST'])
 def extract():
-    return render_template("extract.html")
+    form = ExtractForm(request.form)
+    if form.validate():
+        product_id = form.product_id.data
 
-@app.route("/product/<product_id>")
-def product(product_id):
-    return render_template("product.html", product_id=product_id)
-
-@app.route("/charts/<product_id>")
-def charts(product_id):
-    return render_template("charts.html", product_id=product_id)
+        return redirect(url_for('product', product_id=product_id))
+        else:
+    return render_template('extract.html', form=form)
 
 @app.route("/products")
 def products():
     return render_template("products.html")
+
+@app.route("/product/<product_id>")
+def product(product_id):
+    return render_template("product.html", product_id=product_id)
 
 @app.route("/about")
 def about():
